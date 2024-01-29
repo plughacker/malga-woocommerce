a113456bfe4e2b1e5de0a48a483ecf5d394485ac
Morais Junior <geekdoagrest@gmail.com>
2024-01-29 08:47:04 -0300

    Fix lint

--- src/plugins/woocommerce-malga-payments/templates/notice/missing-clientId.php
+++ src/plugins/woocommerce-malga-payments/templates/notice/missing-clientId.php
@@ -1,4 +1,4 @@
-<?php echo if ( ! defined( 'ABSPATH' ) ) exit; ?>
+<?php if ( ! defined( 'ABSPATH' ) ) exit; ?>
 <div class="error inline">
 	<p><strong><?php esc_html_e( 'Malga Disabled', 'malga-payments-gateway' ); ?></strong>: <?php esc_html_e( 'You should inform your clientId.', 'malga-payments-gateway' ); ?>
 	</p>
